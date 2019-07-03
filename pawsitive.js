71fb782b01a82ea5f082bb48a944ee89bf229d76
Alex <alexpark@pursuit.org>
2019-07-03 16:40:31 -0400

    added svg

--- pawsitive.js
+++ pawsitive.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentBreed
     let currentDogImage 
     let prettyBreedName = ''
+    let isSubBreed = false
 
     const sliceBreedName = (url) => {
         const startingIndex = 30
@@ -20,15 +21,23 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const beautifyName = (name) => {
+        let breedNameArr = []
+        let subBreedArr = []
         for (let i = 0; i < name.length; i++) {
-            if (name[i] === '-') {
-                prettyBreedName += ' '
-            } else if (i === 0 || prettyBreedName[prettyBreedName.length - 1] === ' ') {
-                prettyBreedName += name[i].toUpperCase()
+            if (name[i] === '-') { isSubBreed = true; continue }
+            if (isSubBreed) {
+                if (!subBreedArr.length) {
+                    subBreedArr.push(name[i].toUpperCase())
+                } else if (i === name.length - 1) {
+                    subBreedArr.push(name[i], ' ')
+                } else subBreedArr.push(name[i])
+            } else if (i === 0) {
+                breedNameArr.push(name[i].toUpperCase())
             } else {
-                prettyBreedName += name[i]
+                breedNameArr.push(name[i])
             }
         }
+        prettyBreedName = subBreedArr.join('').concat(breedNameArr.join(''))
     }
 
     const displayMoreDogsButton = () => {
@@ -37,6 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const fetchRandomDog = async () => {
+        isSubBreed = false
         await axios.get('https://dog.ceo/api/breeds/image/random')
         .then(res => {
             currentDogImage = res.data.message
@@ -53,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
         dogImageTag.setAttribute('src', url)
         dogImageTag.setAttribute('alt', 'Random dog')
         if (randomDog.innerHTML) {
-            const oldTag = randomDog.childNodes[1]
+            const oldTag = randomDog.childNodes[0]
             randomDog.replaceChild(dogImageTag, oldTag)
         } else {
             randomDog.appendChild(dogImageTag)
@@ -69,9 +79,40 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
+    const handleSubBreed = () => {
+        let replacedHyphenBreed = currentBreed
+        for (let i = 0; i < currentBreed.length; i++) {
+            if (currentBreed[i] === '-') {
+                return replacedHyphenBreed.slice(0,i) + '/' + replacedHyphenBreed.slice(i + 1)
+            }
+        }
+    }
+
+    const fetchDogByBreed  = async () => {
+        if (isSubBreed) {
+            await axios.get(`https://dog.ceo/api/breed/${handleSubBreed()}/images/random`)
+                .then(res => {
+                    currentDogImage = res.data.message
+                }).catch(err => console.log(err))
+        } else {
+            await axios.get(`https://dog.ceo/api/breed/${currentBreed}/images/random`)
+                .then(res => {
+                    currentDogImage = res.data.message
+                }).catch(err => console.log(err))
+        }
+    }
+
     randomDogButton.addEventListener('click', event => {
         event.preventDefault()
         handleDogButton()
+    })
+
+    moreButton.addEventListener('click', event => {
+        event.preventDefault()
+        fetchDogByBreed()
+        .then(() => {
+            displayDogImage(currentDogImage)
+        })
     })
 
 })
